029c23c1d1114506bf81733a476750f54dfa7245
TamaraZ0777 <tamarazhuk@yahoo.com>
2020-01-06 17:58:41 +0200

    Add project files.

---
+++ ConsolePainter/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsolePainter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Paint paint = new Paint(40, 20);
+            paint.PutRandomNumbers(400);
+            //paint.FillFor(20, 10);
+            //paint.FillQueue(20, 10);
+            //paint.FillStack(20, 10);
+            paint.FillDepth(20, 10);
+            Console.ReadKey();
+        }
+    }
+}
+
